3a11dbb360abd1fe39e5616b23ca4f0dee1bf8f0
this-is-nisa <nisaa2302@gmail.com>
2022-04-10 12:01:08 -0400

    js gets html code

--- Timer.js
+++ Timer.js
@@ -3,4 +3,26 @@ export default class Timer {
 		
 		console.log(root);
 	}
-}
+	
+	static getHTML(){
+		return `
+		
+<span class="timer-part timer-part-hr">00</span>  
+
+<span class="timer-part">:</span>
+<span class="timer-part timer-part-min">00</span>
+<span class="timer-part">:</span>
+<span class="timer-part timer-part-sec">00</span>
+
+<button type="button" class="timer-btn timer-btn-ctrl timer-btn-start"> 
+
+<span class="material-icons">play_arrow</span>
+</button>
+
+<button type="button" class="timer-btn timer-btn-ctrl timer-btn-reset">
+<span class="material-icons">restart_alt</span>
+</button> 
+		
+		`
+	}
+	
