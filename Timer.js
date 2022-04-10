8f33eb00b94e6f35aed39ffb5c4fafd456ab5887
this-is-nisa <nisaa2302@gmail.com>
2022-04-10 12:21:41 -0400

    redo

--- Timer.js
+++ Timer.js
@@ -1,28 +1,2 @@
-export default class Timer {
-	constructor(root) {
-		root.innerHTML = Timer.getHTML();
-		
-	}
-	
-	static getHTML(){
-		return `
-			
-			<span class="timer-part timer-part-hr">00</span>  
 
-			<span class="timer-part">:</span>
-			<span class="timer-part timer-part-min">00</span>
-			<span class="timer-part">:</span>
-			<span class="timer-part timer-part-sec">00</span>
-
-			<button type="button" class="timer-btn timer-btn-ctrl timer-btn-start"> 
-
-			<span class="material-icons">play_arrow</span>
-			</button>
-
-			<button type="button" class="timer-btn timer-btn-ctrl timer-btn-reset">
-			<span class="material-icons">restart_alt</span>
-			</button> 
-		
-		`
-	}
 	
