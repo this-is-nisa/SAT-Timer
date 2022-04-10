6093b6efb1caff9c222f0d5098c230b1bc689fdb
this-is-nisa <nisaa2302@gmail.com>
2022-04-10 12:05:35 -0400

    fix

--- Timer.js
+++ Timer.js
@@ -1,27 +1,27 @@
 export default class Timer {
 	constructor(root) {
+		root.innerHTML = Timer.getHTML();
 		
-		console.log(root);
 	}
 	
 	static getHTML(){
 		return `
-		
-<span class="timer-part timer-part-hr">00</span>  
+			
+			<span class="timer-part timer-part-hr">00</span>  
 
-<span class="timer-part">:</span>
-<span class="timer-part timer-part-min">00</span>
-<span class="timer-part">:</span>
-<span class="timer-part timer-part-sec">00</span>
+			<span class="timer-part">:</span>
+			<span class="timer-part timer-part-min">00</span>
+			<span class="timer-part">:</span>
+			<span class="timer-part timer-part-sec">00</span>
 
-<button type="button" class="timer-btn timer-btn-ctrl timer-btn-start"> 
+			<button type="button" class="timer-btn timer-btn-ctrl timer-btn-start"> 
 
-<span class="material-icons">play_arrow</span>
-</button>
+			<span class="material-icons">play_arrow</span>
+			</button>
 
-<button type="button" class="timer-btn timer-btn-ctrl timer-btn-reset">
-<span class="material-icons">restart_alt</span>
-</button> 
+			<button type="button" class="timer-btn timer-btn-ctrl timer-btn-reset">
+			<span class="material-icons">restart_alt</span>
+			</button> 
 		
 		`
 	}
