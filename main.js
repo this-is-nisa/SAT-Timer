96b6104665cdfe2714a8d46cf26c7473fb590acc
this-is-nisa <nisaa2302@gmail.com>
2022-04-17 00:24:33 -0400

    alarm
    timer now has functional alarm when timer is complete

--- main.js
+++ main.js
@@ -22,6 +22,7 @@ startBtn.addEventListener('click', function(){ // addEventListener and 'click' w
         startTimer = setInterval(function() { // setInterval repeats a function with a fixed time delay (1000)
             timer(); // setinterval is repeating the timer function over and over at 1000
 			fiveMinRem(); // fiveMinRem repeats -- always listening
+			endBeep();
         }, 1000);
     }
     startInterval(); // calling function
@@ -34,14 +35,26 @@ pauseBtn.addEventListener('click', function(){
 	stopInterval()  // defined at bottom
 })
 
+//BEEP WHEN TIMER IS COMPLETE
+var audioBeep = new Audio('https://github.com/this-is-nisa/SAT-Timer/raw/main/timerEndAudio.mp3');
+function endBeep(){
+	if (hr.value == 0 && min.value == 0 && sec.value == 0){
+		audioBeep.play();
+		console.log("almost works");
+	}
+}
+
 // RESET
 resetBtn.addEventListener('click', function() {  
 	hr.value = 0; // sets hr number to 0
 	min.value = 0;
 	sec.value = 0;
 	stopInterval()
+	audioBeep.pause();
+	audioBeep.currentTime = 0;
 
 })
+
 
 // READING
 readingBtn.addEventListener('click', function(){
@@ -72,10 +85,11 @@ calcBtn.addEventListener('click', function(){
 	sec.value = 0;
 	stopInterval()
 })
+
+//FIVE MINUTES REMAING FUNCTION
 var audio = new Audio('https://github.com/this-is-nisa/SAT-Timer/raw/main/Five-Minutes-Remaining-audio-clip.mp3'); // must have link address in github for audio (tutorial: https://www.youtube.com/watch?v=YrTzh2ZeoHM)
 function fiveMinRem() { 
 if (fiveMin.checked && hr.value == 0 && min.value == 5 && sec.value == 0) {
-
 audio.play();
 console.log("it worked"); }
 return;
@@ -87,7 +101,7 @@ function timer(){  // what is being repeated
 if(hr.value == 0 && min.value == 0 && sec.value == 0) {
 	hr.value = 0;
 	min.value = 0;
-	sec.value = 0;		
+	sec.value = 0;
 	}else if(sec.value != 0) {
 	sec.value--;
 } else if(min.value != 0 && sec.value == 0){
